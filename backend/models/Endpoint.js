e9179587c0777cdbb9ed7e982106a06eeed8750d
Kareemunnisa <mailkareemunnisa@gmail.com>
2025-12-19 22:57:57 +0530

    Enhance createEndpoint and getAllEndpoints to properly handle response_body serialization and deserialization

--- backend/models/Endpoint.js
+++ backend/models/Endpoint.js
@@ -4,7 +4,8 @@ export async function createEndpoint({ name, method, url, response_body }) {
   const db = getDB();
   const [result] = await db.execute(
     "INSERT INTO endpoints (name, method, url, response_body) VALUES (?, ?, ?, ?)",
-    [name, method, url, response_body]
+   [name, method, url, JSON.stringify(response_body)]
+
   );
   return result.insertId;
 }
@@ -12,7 +13,21 @@ export async function createEndpoint({ name, method, url, response_body }) {
 export async function getAllEndpoints() {
   const db = getDB();
   const [rows] = await db.execute("SELECT * FROM endpoints");
-  return rows;
+return rows.map(r => {
+  try {
+    return {
+      ...r,
+      response_body: JSON.parse(r.response_body)
+    };
+  } catch {
+    return {
+      ...r,
+      response_body: r.response_body // fallback
+    };
+  }
+});
+
+
 }
 
 export async function getEndpointByUrl(url) {
