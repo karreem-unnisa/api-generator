0348c53b9ef4f38c3d3a373e86fbad0746ddc411
Kareemunnnisa <mailkareemunnisa@gmail.com>
2025-12-19 20:25:41 +0530

    Remove dotenv configuration from server.js
    Removed dotenv import and configuration.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import endpointRoutes from "./routes/endpointRoutes.js";
 import mockRoutes from "./routes/mockRoutes.js";
 
-dotenv.config();
 
 const app = express();
 await connectDB();
